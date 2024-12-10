b8c54a08c0d90e11a40d0244b134cbfcef4f0c49
emma <emma@emma-debian12.gonzalo.com>
2024-12-10 19:13:08 -0300

    homeViewModel pk

--- FrontEnd/Blaze/app/src/main/java/com/emma/blaze/ui/home/Home.java
+++ FrontEnd/Blaze/app/src/main/java/com/emma/blaze/ui/home/Home.java
@@ -1,6 +1,7 @@
 package com.emma.blaze.ui.home;
 
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,19 +14,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import com.emma.blaze.R;
 import com.emma.blaze.adapters.UserAdapter;
 import com.emma.blaze.databinding.FragmentHomeBinding;
-import com.emma.blaze.model.User;
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
 import com.yuyakaido.android.cardstackview.CardStackListener;
-
 import com.yuyakaido.android.cardstackview.Direction;
 import com.yuyakaido.android.cardstackview.StackFrom;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class Home extends Fragment {
@@ -45,79 +41,87 @@ public class Home extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        List<User> users = new ArrayList<>();
-        users.add(new User(1L, "Emma", "john.c.breckinridge@altostrat.com","emma"));
-        users.add(new User(2L, "John", "william.henry.harrison@example-pet-store.com","john"));
-        users.add(new User(1L, "Rousse", "john.c.breckinridge@altostrat.com","rouse"));
-        users.add(new User(2L, "Mica", "william.henry.harrison@example-pet-store.com","mivca"));
-        adapter = new UserAdapter(users, requireContext());
         hViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
-
+        adapter = new UserAdapter(hViewModel.getUsers().getValue(), requireContext());
+        binding.cardStackView.setAdapter(adapter);
 
         manager = new CardStackLayoutManager(requireContext(), new CardStackListener() {
+            @Override
+            public void onCardDragging(Direction direction, float ratio) {
+                View frontCardView = Objects.requireNonNull(binding.cardStackView.getLayoutManager()).findViewByPosition(manager.getTopPosition());
+                if (frontCardView != null) {
+                    hViewModel.swipeColorCard(direction, requireContext());
+                    hViewModel.getHeartColor().observe(getViewLifecycleOwner(), color -> {
+                        ImageView imageHeart = frontCardView.findViewById(R.id.imageheart);
+                        imageHeart.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    });
+                    hViewModel.getCancelColor().observe(getViewLifecycleOwner(), color -> {
+                        ImageView imageCancel = frontCardView.findViewById(R.id.imageCancel);
+                        imageCancel.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    });
+                }
+            }
 
-
-                @Override
-                public void onCardAppeared(View view, int position) {
-                    ImageView imageHeart = view.findViewById(R.id.imageheart);
-                    ImageView imageCancel = view.findViewById(R.id.imageCancel);
-
-                    if (imageHeart != null && imageCancel != null) {
-                        imageHeart.setOnClickListener(v -> {
-
+            @Override
+            public void onCardAppeared(View view, int position) {
+                hViewModel.resetColorCard(requireContext());
+                ImageView imageHeart = view.findViewById(R.id.imageheart);
+                ImageView imageCancel = view.findViewById(R.id.imageCancel);
+                ImageView imageRewind = view.findViewById(R.id.imageRevert);
+                if (imageHeart != null && imageCancel != null && imageRewind != null) {
+                    imageHeart.setOnClickListener(v -> {
+                        hViewModel.swipeColorCard(Direction.Right, requireContext());
+                        hViewModel.getHeartColor().observe(getViewLifecycleOwner(), color -> {
+                            imageHeart.setColorFilter(color, PorterDuff.Mode.SRC_IN);
                             hViewModel.performSwipe(Direction.Right, binding, manager);
                         });
-                        imageCancel.setOnClickListener(v -> {
-                            hViewModel.performSwipe(Direction.Left, binding, manager);
-                        });
-                    }
-                }
+                    });
 
-                @Override
-            public void onCardDragging(Direction direction, float ratio) {
-                hViewModel.swipeColorCard(direction, binding, requireContext(), manager);
-            }
+                imageCancel.setOnClickListener(v -> {
+                    hViewModel.swipeColorCard(Direction.Left, requireContext());
+                    hViewModel.getCancelColor().observe(getViewLifecycleOwner(), color -> {
+                    imageCancel.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    hViewModel.performSwipe(Direction.Left, binding, manager);
+                });
+                });
+                imageRewind.setOnClickListener(v -> {
+                    hViewModel.swipeColorCard(Direction.Bottom, requireContext());
+                    hViewModel.getRewindColor().observe(getViewLifecycleOwner(), color -> {
+                        imageRewind.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                        hViewModel.rewindCard( binding, manager);
+                    });
+                });
+            }}
+
 
             @Override
             public void onCardSwiped(Direction direction) {
-                // Maneja eventos de swipe si es necesario
+                hViewModel.resetColorCard(requireContext());
             }
 
             @Override
-            public void onCardRewound() { }
+            public void onCardRewound() {
+                hViewModel.resetColorCard(requireContext());
+            }
 
             @Override
             public void onCardCanceled() {
-                hViewModel.dropCard(binding, requireContext(), manager);
+                hViewModel.resetColorCard(requireContext());
             }
 
-
-
             @Override
-            public void onCardDisappeared(View view, int position) { }
+            public void onCardDisappeared(View view, int position) {
+
+            }
         });
 
-
-
         manager.setStackFrom(StackFrom.Top);
         manager.setVisibleCount(3);
         manager.setTranslationInterval(8.0f);
         manager.setScaleInterval(0.95f);
-
         binding.cardStackView.setLayoutManager(manager);
 
-
-
-
-        manager.setStackFrom(StackFrom.Top);
-        manager.setVisibleCount(3);
-        manager.setTranslationInterval(8.0f);
-        manager.setScaleInterval(0.95f);
-
-       binding.cardStackView.setLayoutManager(manager);
-        binding.cardStackView.setAdapter(adapter);
-    return binding.getRoot();
+        return binding.getRoot();
     }
-
 
 }
--- FrontEnd/Blaze/app/src/main/java/com/emma/blaze/ui/home/HomeViewModel.java
+++ FrontEnd/Blaze/app/src/main/java/com/emma/blaze/ui/home/HomeViewModel.java
@@ -1,86 +1,109 @@
 package com.emma.blaze.ui.home;
 import android.app.Application;
 import android.content.Context;
-import android.graphics.PorterDuff;
-import android.view.View;
 import android.view.animation.AccelerateInterpolator;
-import android.widget.ImageView;
-
+import android.view.animation.DecelerateInterpolator;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.ViewModel;
-
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import com.emma.blaze.R;
 import com.emma.blaze.databinding.FragmentHomeBinding;
+import com.emma.blaze.model.User;
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
 import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.RewindAnimationSetting;
 import com.yuyakaido.android.cardstackview.SwipeAnimationSetting;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.time.Duration;
-import java.util.Objects;
 
 public class HomeViewModel extends AndroidViewModel {
 
+    private final MutableLiveData<List<User>> users = new MutableLiveData<>();
+    private final MutableLiveData<Integer> heartColor = new MutableLiveData<>();
+    private final MutableLiveData<Integer> cancelColor = new MutableLiveData<>();
+    private final MutableLiveData<Integer> rewindColor = new MutableLiveData<>();
+
     public HomeViewModel(@NonNull Application application) {
         super(application);
+        heartColor.setValue(ContextCompat.getColor(application, R.color.white_opacity));
+        cancelColor.setValue(ContextCompat.getColor(application, R.color.white_opacity));
+        rewindColor.setValue(ContextCompat.getColor(application, R.color.white_opacity));
+        loadUsers();
     }
 
-    public void swipeColorCard(Direction direction, FragmentHomeBinding binding, Context context, CardStackLayoutManager manager) {
-        View frontCardView = Objects.requireNonNull(binding.cardStackView.getLayoutManager()).findViewByPosition(manager.getTopPosition());
+    public LiveData<List<User>> getUsers() {
+        return users;
+    }
 
-        if (frontCardView != null) {
-            ImageView imageHeart = frontCardView.findViewById(R.id.imageheart);
-            ImageView imageCancel = frontCardView.findViewById(R.id.imageCancel);
-            if (imageHeart != null && imageCancel != null) {
-                if (direction == Direction.Right) {
-                    imageHeart.setColorFilter(ContextCompat.getColor(context, R.color.purple_primary), PorterDuff.Mode.SRC_IN);
-                } else if (direction == Direction.Left) {
-                    imageCancel.setColorFilter(ContextCompat.getColor(context, R.color.red_opacity), PorterDuff.Mode.SRC_IN);
-                } else {
-                    imageHeart.setColorFilter(ContextCompat.getColor(context, R.color.white_opacity), PorterDuff.Mode.SRC_IN);
-                    imageCancel.setColorFilter(ContextCompat.getColor(context, R.color.white_opacity), PorterDuff.Mode.SRC_IN);
-                }
-            }
+    public LiveData<Integer> getHeartColor() {
+        return heartColor;
+    }
+
+    public LiveData<Integer> getCancelColor() {
+        return cancelColor;
+    }
+
+    public LiveData<Integer> getRewindColor() {
+        return rewindColor;
+    }
+
+    private void loadUsers() {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1L, "Emma", "john.c.breckinridge@altostrat.com", "emma"));
+        userList.add(new User(2L, "John", "william.henry.harrison@example-pet-store.com", "john"));
+        userList.add(new User(1L, "Rousse", "john.c.breckinridge@altostrat.com", "rouse"));
+        userList.add(new User(2L, "Mica", "william.henry.harrison@example-pet-store.com", "mivca"));
+        users.setValue(userList);
+    }
+
+    public void swipeColorCard(Direction direction, Context context) {
+        if (direction == Direction.Right) {
+            heartColor.setValue(ContextCompat.getColor(context, R.color.purple_primary));
+            cancelColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
+            rewindColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
+        } else if (direction == Direction.Left) {
+            cancelColor.setValue(ContextCompat.getColor(context, R.color.red_opacity));
+            heartColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
+            rewindColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
+        }else if (direction == Direction.Bottom) {
+            rewindColor.setValue(ContextCompat.getColor(context, R.color.black_opacity));
+            heartColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
+            cancelColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
         }
     }
-
     public void performSwipe(Direction direction, FragmentHomeBinding binding, CardStackLayoutManager manager) {
-        View frontCardView = Objects.requireNonNull(binding.cardStackView.getLayoutManager())
-                .findViewByPosition(manager.getTopPosition());
 
-        if (frontCardView != null) {
-            ImageView imageHeart = frontCardView.findViewById(R.id.imageheart);
-            ImageView imageCancel = frontCardView.findViewById(R.id.imageCancel);
-
-            if (imageHeart != null && imageCancel != null) {
-              if(direction==Direction.Right){
-                  imageHeart.setColorFilter(ContextCompat.getColor(getApplication(), R.color.purple_primary), PorterDuff.Mode.SRC_IN);
-              }else {
-                  imageCancel.setColorFilter(ContextCompat.getColor(getApplication(), R.color.red_opacity), PorterDuff.Mode.SRC_IN);
-              }
                 SwipeAnimationSetting setting = new SwipeAnimationSetting.Builder()
                         .setDirection(direction)
                         .setDuration(300)
                         .setInterpolator(new AccelerateInterpolator())
                         .build();
-
                 manager.setSwipeAnimationSetting(setting);
+
 
                 binding.cardStackView.swipe();
             }
-        }
-    }
 
-    public void dropCard(FragmentHomeBinding binding, Context context, CardStackLayoutManager manager) {
-        View frontCardView = Objects.requireNonNull(binding.cardStackView.getLayoutManager()).findViewByPosition(manager.getTopPosition());
-        if (frontCardView != null) {
-            ImageView imageHeart = frontCardView.findViewById(R.id.imageheart);
-            ImageView imageCancel = frontCardView.findViewById(R.id.imageCancel);
-            if (imageHeart != null && imageCancel != null) {
-                imageHeart.setColorFilter(ContextCompat.getColor(context, R.color.white_opacity), PorterDuff.Mode.SRC_IN);
-                imageCancel.setColorFilter(ContextCompat.getColor(context, R.color.white_opacity), PorterDuff.Mode.SRC_IN);
-            }
-        }
+public void rewindCard(FragmentHomeBinding binding, CardStackLayoutManager manager){
+    RewindAnimationSetting setting = new RewindAnimationSetting.Builder()
+            .setDirection(Direction.Bottom)
+            .setDuration(300)
+            .setInterpolator(new DecelerateInterpolator())
+            .build();
+
+    manager.setRewindAnimationSetting(setting);
+
+
+    binding.cardStackView.rewind();
+}
+
+
+    public void resetColorCard(Context context) {
+        heartColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
+        cancelColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
+        rewindColor.setValue(ContextCompat.getColor(context, R.color.white_opacity));
     }
 }
